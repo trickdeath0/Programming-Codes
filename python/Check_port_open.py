7b342cf863c50759ff39c6aec12989205867dea5
trickdeath0 <70007454+trickdeath0@users.noreply.github.com>
2020-08-31 23:25:18 +0300

    Update Check_port_open.py

--- python/Check_port_open.py
+++ python/Check_port_open.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python
 
 from socket import *
-ip =  raw_input("Enter the IP address: ")
-port = input("Enter the port number: ")
+ip =  input("Enter the IP address: ")
+port = int(input("Enter the port number: "))
 sock = socket(AF_INET, SOCK_STREAM)
 if sock.connect_ex((ip, port)):
-    print "Port", port, "is closed"
+    print ("Port", port, "is closed")
 else:
-    print "Port", port, "is open"
+    print ("Port", port, "is open")
