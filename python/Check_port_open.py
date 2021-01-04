98b051e99aedfd86f47c51be15221ea28d5dbf39
trickdeath0 <70007454+trickdeath0@users.noreply.github.com>
2021-01-04 08:44:48 +0200

    Update Check_port_open.py

--- python/Check_port_open.py
+++ python/Check_port_open.py
@@ -1,10 +1,19 @@
 #!/usr/bin/env python
 
 from socket import *
-ip =  input("Enter the IP address: ")
-port = int(input("Enter the port number: "))
+from termcolor import colored
+
 sock = socket(AF_INET, SOCK_STREAM)
-if sock.connect_ex((ip, port)):
-    print ("Port", port, "is closed")
-else:
-    print ("Port", port, "is open")
+setdefaulttimeout(2)
+
+ip =  input("[+] Enter the IP address: ")
+#port = int(input("[+] Enter the port number: "))
+
+def portscanner(port):
+    if sock.connect_ex((ip, port)):
+        print (colored("[!!] Port", port, "is closed"), 'red')
+    else:
+        print (colored("[+] Port", port, "is open"), 'green')
+
+for port in range(1,100):
+    portscanner(port)
